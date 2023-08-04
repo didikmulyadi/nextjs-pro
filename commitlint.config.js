cac2c3fda47a0215be560f1c9ebb1ec3ce2ba930
didikmulyadi <didikmulyadi12@gmail.com>
2023-08-04 22:56:20 +0700

    chore(supported-files): add husky

---
+++ commitlint.config.js
@@ -0,0 +1,71 @@
+import { RuleConfigSeverity } from "@commitlint/types";
+
+/**
+ * CONFIG AND CI/CD CHANGES
+ *   build: development changes, e.g. tsconfig.json
+ *   ci: development changes, e.g. workflow file
+ *
+ * DOCUMENTATION CHANGES
+ *   docs: documentation changes, e.g. readme file or adding a jsdoc
+ *
+ * PRODUCTION CHANGES
+ *   feat: production changes - new feature
+ *   fix: production changes - bug fix
+ *   perf: production changes - improve performance
+ *
+ * DEVELOPMENT CHANGES
+ *   refactor: development changes - neither adds a feature nor fixes a bug
+ *   chore: development changes - the user cant see the changes, e.g. changes in gitignore
+ *   style: development changes - pure styling changes
+ *   test: development changes - affecting any tests
+ */
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-leading-blank": [RuleConfigSeverity.Warning, "always"],
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", 100],
+    "footer-leading-blank": [RuleConfigSeverity.Warning, "always"],
+    "footer-max-line-length": [RuleConfigSeverity.Error, "always", 100],
+    "header-max-length": [RuleConfigSeverity.Error, "always", 100],
+    "scope-case": [RuleConfigSeverity.Error, "always", "lower-case"],
+    "scope-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "page",
+        "shared-component",
+        "layout",
+        "redux",
+        "config",
+        "supported-files",
+        "readme",
+      ],
+    ],
+    "subject-case": [
+      RuleConfigSeverity.Error,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [RuleConfigSeverity.Error, "never"],
+    "subject-full-stop": [RuleConfigSeverity.Error, "never", "."],
+    "type-case": [RuleConfigSeverity.Error, "always", "lower-case"],
+    "type-empty": [RuleConfigSeverity.Error, "never"],
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "build",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "chore",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
