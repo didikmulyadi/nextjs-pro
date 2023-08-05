881dd21e5c65b84613b5d8fa68c637e063af23f4
didikmulyadi <didikmulyadi12@gmail.com>
2023-08-05 09:25:11 +0700

    chore: add data-testid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} data-testid="container__application">
+        {children}
+      </body>
     </html>
   );
 }
